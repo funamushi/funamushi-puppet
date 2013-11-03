8810c5e3bd350fb0db7e565db8d7d28c38a71fc0
f-kubotar <dev@hadashikick.jp>
2013-11-04 02:31:54 +0900

    設定ファイルもpuppet管理

--- modules/maria/manifests/server/config.pp
+++ modules/maria/manifests/server/config.pp
@@ -1,2 +1,23 @@
 class maria::server::config {
+  file { '/etc/my.cnf':
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+    content => template('maria/my.cnf'),
+  }
+
+  file { '/etc/my.cnf.d':
+    ensure => directory,
+    owner  => root,
+    group  => root,
+    mode   => 0755,
+  }
+
+  file { '/etc/my.cnf.d/server.cnf':
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+    content => template('maria/server.cnf'),
+    require => File['/etc/my.cnf.d'],
+  }
 }
