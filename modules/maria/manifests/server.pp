23705bed5753babf981a1f48a6005848a983177a
f-kubotar <dev@hadashikick.jp>
2013-11-04 02:08:26 +0900

    maria 起動

--- modules/maria/manifests/server.pp
+++ modules/maria/manifests/server.pp
@@ -1,3 +1,11 @@
 class maria::server {
+  include maria::yumrepo
   include maria::server::install
+  include maria::server::config
+  include maria::server::service
+
+     Class['maria::yumrepo']
+  -> Class['maria::server::install']
+  -> Class['maria::server::config']
+  ~> Class['maria::server::service']
 }
